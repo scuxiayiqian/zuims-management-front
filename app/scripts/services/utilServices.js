0396f4e09ccaeb4e5543e3a7e93ac1364dbd574c
ase15_xiayq <yiqian.xia@qq.com>
2016-01-07 15:24:54 +0800

    订单数量走势图finished

--- app/scripts/services/utilServices.js
+++ app/scripts/services/utilServices.js
@@ -2,7 +2,7 @@
 
 angular.module('sbAdminApp')
 
-    .service('utilService', function($http, $state, $cookies) {
+    .service('utilService', function($http, $state, $cookies, $filter) {
 
         this.getCurrentToken = function() {
 
@@ -15,6 +15,44 @@ angular.module('sbAdminApp')
 
                 $http.defaults.headers.common['x-auth-token'] = token;
             }
+        };
+
+        this.getOrderCountInfo = function(restid, enddate) {
+            return $http({
+                method: 'GET',
+                url: 'http://202.120.40.175:21104/order/orderCountInfo',
+                params: {
+                    restaurantId: restid,
+                    date: $filter('date')(enddate, 'yyyy-MM-dd')
+                },
+                crossDomain: true
+            });
+        };
+
+        this.formatOrderCountInfo = function(data, whichDataToFormat) {
+            var makeNums = [];
+            var labels = [];
+
+            if (whichDataToFormat == "dorderFinishNum") {
+                for (var i = 0; i < data.length; i++) {
+                    makeNums.push(data[i].dorderFinishNum);
+                    labels.push(i+1);
+                }
+            }
+            else if (whichDataToFormat == "dorderConfirmNum") {
+                for (var i = 0; i < data.length; i++) {
+                    makeNums.push(data[i].dorderConfirmNum);
+                    labels.push(i+1);
+                }
+            }
+            else if (whichDataToFormat == "dorderMakeNum") {
+                for (var i = 0; i < data.length; i++) {
+                    makeNums.push(data[i].dorderMakeNum);
+                    labels.push(i+1);
+                }
+            }
+
+            return [[makeNums], labels];
         };
 
     });
